8ea97055102223a8d1ccb7d8827e38f32356e949
DCabahug1 <150985638+DCabahug1@users.noreply.github.com>
2024-05-10 02:40:20 -0700

    Swiper Fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,32 +20,65 @@ function App() {
       projectURL: "https://dcabahug-dailyplanner.netlify.app/my-planner",
     },
     {
-      imgURL: "",
-      title: "ChatLink",
-      content: ["HTML", "CSS", "JavaScript", "React", "Firebase"],
+      imgURL: "/assets/Project Imgs/DailyPlanner.png",
+      title: "To-Do List",
+      content: ["HTML", "CSS", "JavaScript", "React"],
+      projectURL: "https://dcabahug-dailyplanner.netlify.app/my-planner",
+    },
+    {
+      imgURL: "/assets/Project Imgs/DailyPlanner.png",
+      title: "To-Do List",
+      content: ["HTML", "CSS", "JavaScript", "React"],
+      projectURL: "https://dcabahug-dailyplanner.netlify.app/my-planner",
     },
   ]);
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  const [projectsPerView, setProjectsPerView] = useState(1);
+
+  // Listen for viewport width changes
+  useEffect(() => {
+    console.log(projectsPerView);
+    if (viewportWidth >= 950) {
+      if (projects.length >= 3) {
+        setProjectsPerView(3);
+        document.querySelector(".swiper").style.maxWidth = "1290px";
+        document.querySelector(".swiper-wrapper").style.maxWidth =
+          "1290px";
+        const swiperSlide = document.querySelector(".swiper-slide");
+
+        if (swiperSlide) {
+        }
+      }
+    } else if (viewportWidth >= 700) {
+      if (projects.length >= 2) {
+        setProjectsPerView(2);
+      }
+    } else {
+      setProjectsPerView(1);
+    }
+  }, [viewportWidth]);
 
   // On mount
   useEffect(() => {
     // Event listeners
     // Listen for app scroll
     const app = document.getElementById("app");
-    let viewportHeight = window.innerHeight;
 
-    // Update viewport height on resize
+    // Update viewport width on resize
     window.addEventListener("resize", () => {
-      viewportHeight = window.innerHeight;
+      setViewportWidth(window.innerWidth);
     });
 
     app.addEventListener("scroll", () => {
       console.log(app.scrollTop);
 
       // Adjust scroll indicator
-      const scrollIndicator = document.getElementById("scroll-indicator");
+      const scrollIndicators = document.querySelectorAll(".scroll-indicator");
       let scrollIndOpacity = 1 - app.scrollTop / 500;
 
-      scrollIndicator.style.opacity = scrollIndOpacity;
+      scrollIndicators.forEach((scrollInd) => {
+        scrollInd.style.opacity = scrollIndOpacity;
+      });
 
       // Slide in animation
       if (app.scrollTop >= 200) {
@@ -56,23 +89,30 @@ function App() {
         aboutText.style.right = "0";
       }
 
-      if (app.scrollTop >= 600) {
+      if (app.scrollTop >= 100 && viewportWidth >= 1200) {
+        // Desktop
         const projectsContainer = document.getElementById("projects-container");
 
         projectsContainer.style.opacity = "1";
         projectsContainer.style.top = "0";
-        
+      } else if (app.scrollTop >= 600 && viewportWidth < 1200) {
+        // Mobile
+        const projectsContainer = document.getElementById("projects-container");
+
+        projectsContainer.style.opacity = "1";
+        projectsContainer.style.top = "0";
       }
     });
   }, []);
 
   return (
     <div id="app">
+      {/* Mobile intro & about */}
       <div className="content-container mobile" id="intro-container">
         <img src="/assets/Portrait.png" alt="" id="intro-photo" />
         <h1 id="intro-name">Duane Cabahug</h1>
         <h2 id="intro-major">Computer Science</h2>
-        <div id="scroll-indicator">
+        <div className="scroll-indicator">
           <img src="/assets/Scroll indicator.png" alt="" />
         </div>
       </div>
@@ -87,30 +127,51 @@ function App() {
           innovative solutions and learning.
         </p>
       </div>
+      {/* Desktop intro & about */}
+      <div className="content-container" id="intro-desktop-container">
+        <div id="intro-name-and-major">
+          <img src="/assets/Portrait.png" alt="" id="intro-photo" />
+          <h1>Duane Cabahug</h1>
+          <h2>Computer Science</h2>
+        </div>
+        <p id="intro-text">
+          As a <span> Computer Science major</span> at California State
+          University, Northridge, I bring a dynamic skill set including
+          proficiency in{" "}
+          <span>Java, JavaScript, React JS, HTML, CSS, LuaU, and Python.</span>{" "}
+          My passion for technology and coding drives my continual pursuit of
+          innovative solutions and learning.
+        </p>
+        <div className="scroll-indicator">
+          <img src="/assets/Scroll indicator.png" alt="" />
+        </div>
+      </div>
       <div className="content-container" id="projects-container">
         <h2 id="projects-title">Projects</h2>
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={30}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          {projects.map((project, i) => {
-            return (
-              <SwiperSlide key={"project_" + i}>
-                <Project
-                  imgURL={project.imgURL}
-                  title={project.title}
-                  content={project.content}
-                  projectURL={project.projectURL}
-                />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
+        <div id="swiper-container">
+          <Swiper
+            slidesPerView={projectsPerView}
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            {projects.map((project, i) => {
+              return (
+                <SwiperSlide key={"project_" + i}>
+                  <Project
+                    imgURL={project.imgURL}
+                    title={project.title}
+                    content={project.content}
+                    projectURL={project.projectURL}
+                  />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
       </div>
       <div className="content-container" id="links-container">
         <div id="links-buttons-container">
